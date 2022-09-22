0835b076eb16381d281bb83262305a311072dcff
Giuseppe Tamayo <gtamayoc@ufpso.edu.co>
2022-09-22 00:08:54 -0500

    IMPLEMENTACIÓN - Código base con bootstrap.
    - Creación del .htaccess para la ruta de la página.
    - Cremación de un Release.txt para poder llevar un versionado de la página.
    - Creación de contantes para la base de datos, con PostgreSQL.
    - Integración del framework bootstrap y librerías complementarias al framework.
    - Integración del index.php y login.php para la creación de la vista de login.
    - Integración del footer.php y header.php para la creación de la vista de la página.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SoftwareAgropecuariaVenadillo
 Este proyecto está dedicado exclusivamente para la empresa Agropecuaria Venadillo para la asignatura de Ingeniería de software
+
+
+-
---
+++ librerias/constantes.php
@@ -0,0 +1,12 @@
+<?php
+//session_name();
+//session_start();
+
+const IP_MAQUINA='127.0.0.1';
+const BASE_DE_DATOS='AgropecuariaVenadillo';
+const PUERTO="5432";
+
+const SEL_C='postgres';
+const CLAVE_SEL_C='1234';
+
+?>
